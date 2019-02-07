bd530512f6e34d619a15ab570e2f8f737138457d
Vargason <jzl4g6@sdcounty.ca.gov>
2019-02-07 13:46:29 -0800

    functionized the Daily Monitoring Script

--- ParseDailyMonitoring.ps1
+++ ParseDailyMonitoring.ps1
@@ -93,109 +93,138 @@ function Get-ParsedDailyReport() {
     return $resultArray
 }
 
+function Set-ArchiveOutputFiles {
+    $outputFiles = Get-ChildItem -Path .\output | Where-Object {$_.Name -like "*.csv"}
 
-$emailTexts = Get-ChildItem -Path .\EmailTexts | Where {$_.Name -like "*.txt"}
-$count = $emailTexts.Count
-$i = 0
+    $archiveMonth = (Get-Date (Get-Date).AddMonths(-2) -format "MM")
 
-foreach ($file in $emailTexts) {
-    $output = Get-ParsedDailyReport -fileInput (Get-Content -Path $file.FullName)
-
-    $timeRan = $output.Run_Date | Select -First 1 | Out-String
-
-    try {
-        $timeOut = [DateTime]::Parse($timeRan)
-
-        $timeOut = $timeOut.ToString("MMddyy-HHmm")
-
-        if ($timeOut.Contains("0800") -or $timeOut.Contains("1700") -or $timeOut.Contains("2300")) {
-        
-            $fileNameSplit = $file.Name.Split("_")
-
-            $serverInName = $fileNameSplit[0]
-
-            $fileOutputName = ".\output\BUR_Summary_$serverInName" + "_$timeOut.csv"
-
-            $output | Export-Csv -Path $fileOutputName -NoTypeInformation
-        }
-    }
-    catch {
-        Write-Host "Issue with fileName: " $file.Name -ForegroundColor Yellow
+    if (Test-Path -Path .outputSummary.xlsx) {
+        Remove-Item -Path .\outputSummary.xlsx
     }
 
-    $i++
-    Write-Progress -Activity "Getting BUR output" -Status "Completed output for $fileOutputName" -PercentComplete (($i / $count) * 100)
+    if (!(Test-Path -Path .\archive\$archiveMonth)) {
+        New-Item -ItemType Directory -Path .\archive\$archiveMonth
+    }
+
+    foreach ($file in $outputFiles) {
+
+        Copy-Item -Path $file.FullName -Destination .\archive\$archiveMonth
+        Remove-Item $file.FullName
+    }
 }
 
+function Get-OutputFiles {
+    $emailTexts = Get-ChildItem -Path .\EmailTexts | Where-Object {$_.Name -like "*.txt"}
+    $count = $emailTexts.Count
+    $i = 0
 
 
-$files = Get-ChildItem -Path .\output
+    foreach ($file in $emailTexts) {
+        $output = Get-ParsedDailyReport -fileInput (Get-Content -Path $file.FullName)
 
-$hash = @{}
+        $timeRan = $output.Run_Date | Select-Object -First 1 | Out-String
+
+        try {
+            $timeOut = [DateTime]::Parse($timeRan)
+
+            $timeOut = $timeOut.ToString("MMddyy-HHmm")
+
+            if (($timeOut.Contains("0800") -or $timeOut.Contains("1700") -or $timeOut.Contains("2300")) -and $timeOut.StartsWith((Get-Date (Get-Date).AddMonths(-1) -format "MM"))) {
+            
+                $fileNameSplit = $file.Name.Split("_")
+
+                $serverInName = $fileNameSplit[0]
+
+                $fileOutputName = ".\output\BUR_Summary_$serverInName" + "_$timeOut.csv"
+
+                $output | Export-Csv -Path $fileOutputName -NoTypeInformation
+            }
+        }
+        catch {
+            Write-Host "Issue with fileName: " $file.Name -ForegroundColor Yellow
+        }
+
+        $i++
+        Write-Progress -Activity "Getting BUR output" -Status "Completed output for $fileOutputName" -PercentComplete (($i / $count) * 100)
+    }
+}
+
+function Get-MonthlyBURReport {
+
+    Set-ArchiveOutputFiles
+
+    Get-OutputFiles
+
+    $files = Get-ChildItem -Path .\output
+
+    $hash = @{}
 
 
-foreach ($file in $files) {
-    $date = $file.Name.Substring($file.Name.Length - 15).Replace(".csv","")
-    $data = Import-Csv -Path $file.FullName
-    $numFailed = $data | Where {$_.FailedDA -gt 0}
-    $totalClients = $data.Count
-    $numFailed = $numFailed.Count
+    foreach ($file in $files) {
+        $date = $file.Name.Substring($file.Name.Length - 15).Replace(".csv","")
+        $data = Import-Csv -Path $file.FullName
+        $numFailed = $data | Where-Object {$_.FailedDA -gt 0}
+        $totalClients = $data.Count
+        $numFailed = $numFailed.Count
 
-    if ($numFailed -eq $null ) {
-        $numFailed = 0
+        if ($null -eq $numFailed) {
+            $numFailed = 0
+        }
+
+        if ($hash.ContainsKey($date)) {
+
+
+            $hash[$date].TotalClients = $totalClients + $hash[$date].TotalClients
+            $hash[$date].TotalClientsSuccessful = ($totalClients - $numFailed) + $hash[$date].TotalClientsSuccessful
+            $hash[$date].TotalClientsFailed = $numFailed + $hash[$date].TotalClientsFailed
+            $hash[$date].FilesParsed += 1
+        }
+        else {
+            $obj = New-Object -TypeName PsObject
+
+            $obj | Add-Member -Name "TotalClients" -Value $totalClients -MemberType NoteProperty
+            $obj | Add-Member -Name "TotalClientsSuccessful" -Value ($totalClients - $numFailed) -MemberType NoteProperty
+            $obj | Add-Member -Name "TotalClientsFailed" -Value $numFailed -MemberType NoteProperty
+            $obj | Add-Member -Name "FilesParsed" -Value 1 -MemberType NoteProperty
+
+            $hash.Add($date, $obj)
+
+            Write-Host $numFailed,$hash[$date].TotalClientsFailed,$file.Name
+        }
+
     }
 
-    if ($hash.ContainsKey($date)) {
+    $resArray = @()
 
-
-        $hash[$date].TotalClients = $totalClients + $hash[$date].TotalClients
-        $hash[$date].TotalClientsSuccessful = ($totalClients - $numFailed) + $hash[$date].TotalClientsSuccessful
-        $hash[$date].TotalClientsFailed = $numFailed + $hash[$date].TotalClientsFailed
-        $hash[$date].FilesParsed += 1
-    }
-    else {
+    foreach ($key in $hash.Keys) {
         $obj = New-Object -TypeName PsObject
 
-        $obj | Add-Member -Name "TotalClients" -Value $totalClients -MemberType NoteProperty
-        $obj | Add-Member -Name "TotalClientsSuccessful" -Value ($totalClients - $numFailed) -MemberType NoteProperty
-        $obj | Add-Member -Name "TotalClientsFailed" -Value $numFailed -MemberType NoteProperty
-        $obj | Add-Member -Name "FilesParsed" -Value 1 -MemberType NoteProperty
+        $obj | Add-Member -Name "DateTime" -Value $key -MemberType NoteProperty
+        $obj | Add-Member -Name "TotalClients" -Value ($hash[$key].TotalClients) -MemberType NoteProperty
+        $obj | Add-Member -Name "TotalClientsSuccessful" -Value $hash[$key].TotalClientsSuccessful -MemberType NoteProperty
+        $obj | Add-Member -Name "TotalClientsFailed" -Value $hash[$key].TotalClientsFailed -MemberType NoteProperty
+        $obj | Add-Member -Name "FilesParsed" -Value $hash[$key].FilesParsed -MemberType NoteProperty
+        $obj | Add-Member -Name "DailySuccessAverage" -Value ([math]::Round(((($hash[$key].TotalClientsSuccessful) / $hash[$key].TotalClients) * 100), 2)) -MemberType NoteProperty
 
-        $hash.Add($date, $obj)
-
-        Write-Host $numFailed,$hash[$date].TotalClientsFailed,$file.Name
+        $resArray += $obj
     }
 
-}
 
-$resArray = @()
+    foreach ($row in $resArray) {
+        $row.DateTime = "$($row.DateTime.Substring(0,2))/$($row.DateTime.Substring(2,2))/$($row.DateTime.Substring(4,2))"
+    }
 
-foreach ($key in $hash.Keys) {
-    $obj = New-Object -TypeName PsObject
+    $resGroups = $resArray | Group-Object -Property DateTime
 
-    $obj | Add-Member -Name "DateTime" -Value $key -MemberType NoteProperty
-    $obj | Add-Member -Name "TotalClients" -Value ($hash[$key].TotalClients) -MemberType NoteProperty
-    $obj | Add-Member -Name "TotalClientsSuccessful" -Value $hash[$key].TotalClientsSuccessful -MemberType NoteProperty
-    $obj | Add-Member -Name "TotalClientsFailed" -Value $hash[$key].TotalClientsFailed -MemberType NoteProperty
-    $obj | Add-Member -Name "FilesParsed" -Value $hash[$key].FilesParsed -MemberType NoteProperty
-    $obj | Add-Member -Name "DailySuccessAverage" -Value ([math]::Round(((($hash[$key].TotalClientsSuccessful) / $hash[$key].TotalClients) * 100), 2)) -MemberType NoteProperty
+    $newOutput = @()
 
-    $resArray += $obj
-}
+    foreach ($group in $resGroups) {
 
+        $newOutput += $group.Group | Sort-Object -Property DailySuccessAverage -Descending | Select-Object -First 1
+    }
 
-foreach ($row in $resArray) {
-    $row.DateTime = "$($row.DateTime.Substring(0,2))/$($row.DateTime.Substring(2,2))/$($row.DateTime.Substring(4,2))"
-}
-
-$resGroups = $resArray | Group-Object -Property DateTime
-
-$newOutput = @()
-
-foreach ($group in $resGroups) {
-
-    $newOutput += $group.Group | Sort-Object -Property DailySuccessAverage -Descending | Select -First 1
+    $newOutput
 }
 
 
-$newOutput | Export-Excel -Path .\outputSummary.xlsx -WorkSheetname "DailySuccessAVG" -TableName "DateSuccessAVG" -Show
+Get-MonthlyBURReport | Export-Excel -Path .\outputSummary.xlsx -WorkSheetname "DailySuccessAVG" -TableName "DateSuccessAVG" -Show

8882ec94a381a343295331a2ff7160be41e11b20
= <=>
2019-04-09 09:12:27 -0700

    added comments and refactored code in standard parsing script

--- ParseDailyMonitoring.ps1
+++ ParseDailyMonitoring.ps1
@@ -35,20 +35,35 @@ function Get-ParsedDailyReport() {
 
      #>
     
-    #save the header
-    $header = $fileInput[0..11]
+    #create variables for header and the date
+    $header = ""
+    $date = ""
+    $startRow = 0
 
-    #get the date from the header
-    $date = $header[3]
+    if ($fileInput[0][0] -eq "#") {
+        $header = $fileInput[0..8]
+        $date = $header[2]
+        $startRow = 9
+    }
+    else {
+        #save the header
+        $header = $fileInput[0..11]
+
+        #get the date from the header
+        $date = $header[3]
+        $startRow = 12
+    }
 
     #remove the beginning of Date row
     $date = $date.Remove(0, 15)
+
+    $date = $date.Trim()
 
     #create a result array
     $resultArray = @()
 
     #skip the header and read the rows of the text
-    for ($i = 12; $i -lt $fileInput.Count; $i++) {
+    for ($i = $startRow; $i -lt $fileInput.Count; $i++) {
 
         #get the row
         $row = $fileInput[$i]
@@ -93,19 +108,26 @@ function Get-ParsedDailyReport() {
     return $resultArray
 }
 
+# function to move the archive files to archive directory and remove old report data
 function Set-ArchiveOutputFiles {
+
+    # get the output files in the output directory
     $outputFiles = Get-ChildItem -Path .\output | Where-Object {$_.Name -like "*.csv"}
 
+    # get the month we are archiving for
     $archiveMonth = (Get-Date (Get-Date).AddMonths(-2) -format "MM")
 
-    if (Test-Path -Path .outputSummary.xlsx) {
+    # remove the old report data
+    if (Test-Path -Path .\outputSummary.xlsx) {
         Remove-Item -Path .\outputSummary.xlsx
     }
 
+    # create the archive month folder under archive
     if (!(Test-Path -Path .\archive\$archiveMonth)) {
-        New-Item -ItemType Directory -Path .\archive\$archiveMonth
+        $null = New-Item -ItemType Directory -Path .\archive\$archiveMonth 
     }
 
+    # move the output files to the created archive folder
     foreach ($file in $outputFiles) {
 
         Copy-Item -Path $file.FullName -Destination .\archive\$archiveMonth
@@ -113,30 +135,49 @@ function Set-ArchiveOutputFiles {
     }
 }
 
+# function to produce output files from parsed text files
 function Get-OutputFiles {
+
+    # set the email texts to the emailed text files downloaded to the specific folder
     $emailTexts = Get-ChildItem -Path .\EmailTexts | Where-Object {$_.Name -like "*.txt"}
+
+    # save the count to help with iterating performance
     $count = $emailTexts.Count
+
+    # initialize a counter at 0
     $i = 0
 
-
+    # loop through the emailed files
     foreach ($file in $emailTexts) {
+
+        # get the output based on the imported text file content
         $output = Get-ParsedDailyReport -fileInput (Get-Content -Path $file.FullName)
 
+        # get the run date from the output result
         $timeRan = $output.Run_Date | Select-Object -First 1 | Out-String
 
+        # try catch for summary parsing
         try {
+            # parse timeout into a datetime
             $timeOut = [DateTime]::Parse($timeRan)
 
+            # specify a datetime string format
             $timeOut = $timeOut.ToString("MMddyy-HHmm")
 
+            # filter to make sure we are getting correctly timed files
             if (($timeOut.Contains("0800") -or $timeOut.Contains("1700") -or $timeOut.Contains("2300")) -and $timeOut.StartsWith((Get-Date (Get-Date).AddMonths(-1) -format "MM"))) {
-            
+                
+                # get the array split based on splitting the file name
                 $fileNameSplit = $file.Name.Split("_")
 
+                # get the server in the name from the filename split
+                #TODO: This could be condensed into a single line
                 $serverInName = $fileNameSplit[0]
 
+                # specify the new file output name exported as a csv
                 $fileOutputName = ".\output\BUR_Summary_$serverInName" + "_$timeOut.csv"
 
+                # export the csv without type information
                 $output | Export-Csv -Path $fileOutputName -NoTypeInformation
             }
         }
@@ -144,42 +185,59 @@ function Get-OutputFiles {
             Write-Host "Issue with fileName: " $file.Name -ForegroundColor Yellow
         }
 
+        # increase the counter and write to the progress bar
         $i++
         Write-Progress -Activity "Getting BUR output" -Status "Completed output for $fileOutputName" -PercentComplete (($i / $count) * 100)
     }
 }
 
+# function to get the monthly bur report from calling needed functions
 function Get-MonthlyBURReport {
 
+    # set the archived output files
     Set-ArchiveOutputFiles
 
+    # get the output files
     Get-OutputFiles
-
+    
+    # get the output files in the output folder
     $files = Get-ChildItem -Path .\output
 
+    # initialize a new hash
     $hash = @{}
 
-
+    # loop through the files and summarize
     foreach ($file in $files) {
+        # date can be gathered from the filename substring 
         $date = $file.Name.Substring($file.Name.Length - 15).Replace(".csv","")
+
+        # import the spreadsheet
         $data = Import-Csv -Path $file.FullName
+
+        # get the num failed objects
         $numFailed = $data | Where-Object {$_.FailedDA -gt 0}
+
+        # count total variables for average calculations
         $totalClients = $data.Count
         $numFailed = $numFailed.Count
 
+        # null check
         if ($null -eq $numFailed) {
             $numFailed = 0
         }
 
+        # if the hash contains the date key then fill out the hash data accordingly
+        # else create a new object and add to the hash representation
         if ($hash.ContainsKey($date)) {
-
 
             $hash[$date].TotalClients = $totalClients + $hash[$date].TotalClients
             $hash[$date].TotalClientsSuccessful = ($totalClients - $numFailed) + $hash[$date].TotalClientsSuccessful
             $hash[$date].TotalClientsFailed = $numFailed + $hash[$date].TotalClientsFailed
             $hash[$date].FilesParsed += 1
+
         }
         else {
+
             $obj = New-Object -TypeName PsObject
 
             $obj | Add-Member -Name "TotalClients" -Value $totalClients -MemberType NoteProperty
@@ -194,8 +252,10 @@ function Get-MonthlyBURReport {
 
     }
 
+    # intialize an empty array
     $resArray = @()
 
+    # loop through the hash to fill out the summary array with custom members
     foreach ($key in $hash.Keys) {
         $obj = New-Object -TypeName PsObject
 
@@ -209,22 +269,26 @@ function Get-MonthlyBURReport {
         $resArray += $obj
     }
 
-
+    # update datetime of the row data
     foreach ($row in $resArray) {
         $row.DateTime = "$($row.DateTime.Substring(0,2))/$($row.DateTime.Substring(2,2))/$($row.DateTime.Substring(4,2))"
     }
 
+    # group the data
     $resGroups = $resArray | Group-Object -Property DateTime
 
+    # initialize a new empty array
     $newOutput = @()
 
+    # add each group to the output
     foreach ($group in $resGroups) {
 
         $newOutput += $group.Group | Sort-Object -Property DailySuccessAverage -Descending | Select-Object -First 1
     }
 
-    $newOutput
+    # output the new output
+    Write-Output $newOutput
 }
 
-
+# final funciton calls
 Get-MonthlyBURReport | Export-Excel -Path .\outputSummary.xlsx -WorkSheetname "DailySuccessAVG" -TableName "DateSuccessAVG" -Show
